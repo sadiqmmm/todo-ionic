6de9188737c6865e0fe280d7a89d8f2921943073
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-03-25 16:40:42 +0530

    Todo feature

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public todos = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
   }
-
+  openTodoAlert() {
+    let addTodoAlert = this.alertCtrl.create({
+      title: "Add a todo",
+      message: "Enter your todo",
+      inputs: [{
+        type: "text",
+        name: "addTodoInput"
+      }],
+      buttons: [{
+        text: "Cancel"
+      },
+      {
+        text: "Add Todo",
+        handler: (inputData) => {
+          let todoText;
+          todoText = inputData.addTodoInput;
+          this.todos.push(todoText);
+        }
+      }]
+    });
+    addTodoAlert.present();
+  }
 }
